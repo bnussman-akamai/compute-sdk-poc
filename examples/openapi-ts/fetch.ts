4c97104aa28155ad524d516bdc7f419547300bcb
Banks Nussman <banks@nussman.us>
2025-12-09 15:21:29 -0500

    chore: test pagination params in playground

--- examples/openapi-ts/fetch.ts
+++ examples/openapi-ts/fetch.ts
@@ -1,4 +1,4 @@
-import { getLinodeInstance } from '@akamai/openapi-ts'
+import { getLinodeInstance, getLinodeInstances } from '@akamai/openapi-ts'
 import { client } from "@akamai/openapi-ts/client";
 
 client.setConfig({
@@ -10,3 +10,7 @@ client.setConfig({
 const linode = await getLinodeInstance({ path: { linodeId: 123 }});
 
 console.log("Linode Response:", linode)
+
+const linodes = await getLinodeInstances({ query: { page: 1, page_size: 125 } });
+
+console.log("Linodes Response:", linodes.data)
